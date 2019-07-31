5c8dfbd097048f844efc7045a3bbd02da218517c
Kevin Johnson <kevin@coolkev.com>
2019-07-30 20:34:56 -0400

    Use improved technique based on display_aspect_ratio

--- thumbs/video-thumb.js
+++ thumbs/video-thumb.js
@@ -2,78 +2,80 @@ const ffmpeg = require("fluent-ffmpeg");
 
 const ThumbnailSupplier = require("../thumb.js");
 
-function ratioStringToNumber(str) {
-    const [numerator, denominator] = str
-        .split(":")
-        .map(n => parseInt(n, 10));
-    return numerator / denominator;
+function ratioStringToParts(str) {
+  return str.split(":").map(n => parseInt(n, 10));
 }
 
 class VideoThumbnailSupplier extends ThumbnailSupplier {
+  constructor(options) {
+    super(options);
+    this.timestamp = options.timestamp || "10%";
+  }
 
-    constructor(options) {
-        super(options);
-        this.timestamp = options.timestamp || "10%";
-    }
+  createThumbnail(video) {
+    return new Promise((resolve, reject) => {
+      const hash = ThumbnailSupplier.hashFile(video);
 
-    createThumbnail(video) {
-        return new Promise((resolve, reject) => {
-            const hash = ThumbnailSupplier.hashFile(video);
-
-            this.getVideoDimension(video)
-                .then(this.getOptimalThumbnailResolution.bind(this))
-                .then(res => {
-                    ffmpeg(video)
-                        .on("end", () => resolve(super.getThumbnailLocation(video)))
-                        .on("error", reject)
-                        .screenshots({
-                            size: `${res.width}x${res.height}`,
-                            timestamps: [this.timestamp],
-                            filename: ThumbnailSupplier.getThumbnailFileName(video),
-                            folder: this.cacheDir
-                        });
-                })
-                .catch(err => {
-                    ffmpeg(video)
-                        .on("end", () => resolve(super.getThumbnailLocation(video)))
-                        .on("error", reject)
-                        .screenshots({
-                            size: `${this.size.width}x${this.size.height}`,
-                            timestamps: [this.timestamp],
-                            filename: ThumbnailSupplier.getThumbnailFileName(video),
-                            folder: this.cacheDir
-                        });
-                });
+      this.getVideoDimension(video)
+        .then(this.getOptimalThumbnailResolution.bind(this))
+        .then(res => {
+          ffmpeg(video)
+            .on("end", () => resolve(super.getThumbnailLocation(video)))
+            .on("error", reject)
+            .screenshots({
+              size: `${res.width}x${res.height}`,
+              timestamps: [this.timestamp],
+              filename: ThumbnailSupplier.getThumbnailFileName(video),
+              folder: this.cacheDir
+            });
+        })
+        .catch(err => {
+          ffmpeg(video)
+            .on("end", () => resolve(super.getThumbnailLocation(video)))
+            .on("error", reject)
+            .screenshots({
+              size: `${this.size.width}x${this.size.height}`,
+              timestamps: [this.timestamp],
+              filename: ThumbnailSupplier.getThumbnailFileName(video),
+              folder: this.cacheDir
+            });
         });
-    }
+    });
+  }
 
-    getVideoDimension(video) {
-        return new Promise((resolve, reject) => {
-            ffmpeg.ffprobe(video, (err, metadata) => {
-                if (err) return reject(err);
-                const sar = ratioStringToNumber(metadata.streams[0].sample_aspect_ratio);
+  getVideoDimension(video) {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video, (err, metadata) => {
+        if (err) return reject(err);
+        const [widthRatioPart, heightRatioPart] = ratioStringToParts(
+          metadata.streams[0].display_aspect_ratio
+        );
 
-                resolve({
-                    width: metadata.streams[0].width * sar,
-                    height: metadata.streams[0].height
-                });
-            })
+        resolve({
+          width: metadata.streams[0].width,
+          height: metadata.streams[0].width * (heightRatioPart / widthRatioPart)
         });
-    }
+      });
+    });
+  }
 
-    getOptimalThumbnailResolution(videoDimension) {
-        if(videoDimension.width > videoDimension.height) {
-            return {
-                width: this.size.width,
-                height: Math.round(this.size.width * videoDimension.height / videoDimension.width)
-            }
-        } else {
-            return {
-                width: Math.round(this.size.height * videoDimension.width / videoDimension.height),
-                height: this.size.height
-            }
-        }
+  getOptimalThumbnailResolution(videoDimension) {
+    if (videoDimension.width > videoDimension.height) {
+      return {
+        width: this.size.width,
+        height: Math.round(
+          (this.size.width * videoDimension.height) / videoDimension.width
+        )
+      };
+    } else {
+      return {
+        width: Math.round(
+          (this.size.height * videoDimension.width) / videoDimension.height
+        ),
+        height: this.size.height
+      };
     }
+  }
 }
 
 module.exports = VideoThumbnailSupplier;
